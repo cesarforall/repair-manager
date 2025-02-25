e5962f9152c2c5f0270997d47f1bebb8e6f0ea31
Cesar Almeida <cesaralmeida89@gmail.com>
2025-02-25 22:10:48 +0100

    add method save to cliente dao

---
+++ src/main/java/dao/ClienteDAO.java
@@ -0,0 +1,50 @@
+package dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import models.Cliente;
+import utils.HibernateUtil;
+
+public class ClienteDAO implements GenericDAO<Cliente>{
+	private Session getSession() {
+		return HibernateUtil.getSessionFactory().openSession();
+	}
+
+	@Override
+	public void save(Cliente cliente) {
+		Session session = getSession();
+		Transaction transaction = session.beginTransaction();
+		
+		session.persist(cliente);
+		
+		transaction.commit();
+		session.close();		
+	}
+
+	@Override
+	public void update(Cliente entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Cliente entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Cliente findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Cliente> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
