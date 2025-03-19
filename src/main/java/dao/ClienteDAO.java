e9bc525b1b2d720894e331522d0a024c5cf89b76
Cesar Almeida <cesaralmeida89@gmail.com>
2025-03-19 19:14:20 +0100

    Refactor dao. Individualize session managment in each method

--- src/main/java/dao/ClienteDAO.java
+++ src/main/java/dao/ClienteDAO.java
@@ -9,35 +9,41 @@ import model.Cliente;
 import util.HibernateUtil;
 
 public class ClienteDAO implements GenericDAO<Cliente>{
-	private Session getSession() {
-		return HibernateUtil.getSessionFactory().openSession();
-	}
-
 	@Override
-	public void save(Cliente cliente) {		
-		try (Session session = getSession()) {
-			Transaction transaction = session.beginTransaction();
-			
-			session.persist(cliente);
-			
+	public void save(Cliente cliente) {
+		Transaction transaction = null;		
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			transaction = session.beginTransaction();			
+			session.persist(cliente);			
 			transaction.commit();
 		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
 			System.err.println("Error en dao.");
 			e.printStackTrace();
 		}		
 	}
 
 	@Override
-	public void update(Cliente entity) {
-		// TODO Auto-generated method stub
-		
+	public void update(Cliente cliente) {
+		Transaction transaction = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			transaction = session.beginTransaction();
+			session.merge(cliente);
+			transaction.commit();			
+		} catch (Exception e) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			// Error message
+		}		
 	}
 
 	@Override
 	public void delete(Cliente cliente) {
-		Transaction transaction = null;
-		
-		try (Session session = getSession()) {
+		Transaction transaction = null;		
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
 			transaction = session.beginTransaction();
 			session.remove(cliente);			
 			transaction.commit();
@@ -47,32 +53,31 @@ public class ClienteDAO implements GenericDAO<Cliente>{
 			}
 			System.err.println("Error en dao.");
 			e.printStackTrace();
-		}
-		
+		}		
 	}
 
 	@Override
 	public Cliente findById(int id) {
-		Session session = getSession();
-		Cliente cliente = session.get(Cliente.class, id);
-		session.close();
+		Cliente cliente = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			cliente = session.get(Cliente.class, id);			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 		return cliente;
 	}
 
 	@Override
 	public List<Cliente> findAll() {
 		List<Cliente> clientes = null;
-		
-		try (Session session = getSession();) {
-			Transaction transaction = session.beginTransaction();
-			
-			clientes = session.createQuery("FROM CLIENTES", Cliente.class).getResultList();
-						
+		Transaction transaction = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			transaction = session.beginTransaction();			
+			clientes = session.createQuery("FROM CLIENTES", Cliente.class).getResultList();						
 			transaction.commit();
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		
+		}		
 		return clientes;
 	}
 }
