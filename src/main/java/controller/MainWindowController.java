60bc31664719a5980d29cf7c9ed82cff940b05d7
César Almeida <cesaralmeida89@gmail.com>
2025-05-23 18:21:45 +0200

    Refactor: extract tab opening into reusable method

--- src/main/java/controller/MainWindowController.java
+++ src/main/java/controller/MainWindowController.java
@@ -26,145 +26,57 @@ public class MainWindowController {
 
     @FXML
     public void abrirClientes() {
-        try {
-        	FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/clientesView.fxml"));
-        	Parent tablaClientes = loader.load();
-        	
-        	ClientesViewController clientesViewController = loader.getController();
-        	clientesViewController.setStatusCallback(this::setStatusMessage);
-
-            Tab nuevaPestaña = new Tab("Clientes");
-            nuevaPestaña.setContent(tablaClientes);
-            
-            mainTabPanel.getTabs().add(nuevaPestaña);
-            mainTabPanel.getSelectionModel().select(nuevaPestaña);
-            
-            
-            
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    	openTab("Clientes", "/views/clientesView.fxml", true);
     }
     
     @FXML
     public void abrirFormCliente() {
-        for (Tab tab : mainTabPanel.getTabs()) {
-            if (tab.getText().equals("Añadir Cliente")) {
-                mainTabPanel.getSelectionModel().select(tab);
-                return;
-            }
-        }
-        
-    	try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/clienteForm.fxml"));
-			Parent formCliente = loader.load();
-			
-			Tab formClienteTab = new Tab("Añadir Cliente");
-			formClienteTab.setContent(formCliente);
-			
-			mainTabPanel.getTabs().add(formClienteTab);
-			mainTabPanel.getSelectionModel().select(formClienteTab);
-		} catch (IOException e) {
-			System.err.println("Error al abrir el formulario de Cliente.");
-			e.printStackTrace();
-		}
+    	openTab("Nuevo Cliente", "/views/clienteForm.fxml", false);
     }
     
     @FXML
     public void openDeviceForm() {
-    	for (Tab tab : mainTabPanel.getTabs()) {
-            if (tab.getText().equals("Añadir Dispositivo")) {
-                mainTabPanel.getSelectionModel().select(tab);
-                return;
-            }
-    	}
-    	
-    	try {
-    		FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/dispositivoForm.fxml"));
-    		Parent dispositivoForm = loader.load();
-    		
-    		Tab dispositivoFormTab = new Tab("Añadir Dispositivo");
-    		dispositivoFormTab.setContent(dispositivoForm);
-    		
-    		mainTabPanel.getTabs().add(dispositivoFormTab);
-    		mainTabPanel.getSelectionModel().select(dispositivoFormTab);
-    	} catch (IOException e) {
-    		System.err.println("Error al abrir el formulario de Dispositivo.");
-			e.printStackTrace();
-		}
+    	openTab("Nuevo Dispositivo", "/views/dispositivoForm.fxml", false);
     }        
     
     @FXML
     public void openRepuestosView() {
-        for (Tab tab : mainTabPanel.getTabs()) {
-            if (tab.getText().equals("Gestionar Repuestos")) {
-                mainTabPanel.getSelectionModel().select(tab);
-                return;
-            }
-        }
-        
-    	try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/repuestosView.fxml"));
-			Parent repuestosView = loader.load();
-			
-			Tab repuestosViewTab = new Tab("Gestionar Repuestos");
-			repuestosViewTab.setContent(repuestosView);
-			
-			mainTabPanel.getTabs().add(repuestosViewTab);
-			mainTabPanel.getSelectionModel().select(repuestosViewTab);
-		} catch (IOException e) {
-			System.err.println("Error al abrir la vista de Repuestos.");
-			e.printStackTrace();
-		}
+    	openTab("Nuevo Repuesto", "/views/repuestosView.fxml", false);
     }
     
     @FXML
     public void openEstadosView() {
-        for (Tab tab : mainTabPanel.getTabs()) {
-            if (tab.getText().equals("Configurar Estados")) {
-                mainTabPanel.getSelectionModel().select(tab);
-                return;
-            }
-        }
-        
-    	try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/estadosView.fxml"));
-			Parent estadosView = loader.load();
-			
-			Tab estadosViewTab = new Tab("Configurar Estados");
-			estadosViewTab.setContent(estadosView);
-			
-			mainTabPanel.getTabs().add(estadosViewTab);
-			mainTabPanel.getSelectionModel().select(estadosViewTab);
-		} catch (IOException e) {
-			System.err.println("Error al abrir la vista de Estados.");
-			e.printStackTrace();
-		}
+    	openTab("Nuevo Estado", "/views/estadosView.fxml", false);
     }
     
     @FXML
     public void openReparacionForm() {
-        for (Tab tab : mainTabPanel.getTabs()) {
-            if (tab.getText().equals("Añadir Reparación")) {
-                mainTabPanel.getSelectionModel().select(tab);
-                return;
-            }
-        }
-        
+    	openTab("Nueva Reparación", "/views/reparacionesFormView.fxml", false);
+    }
+    
+    private void openTab(String title, String fxmlPath, boolean allowMultiple) {
+    	if (!allowMultiple) {
+    		for (Tab tab : mainTabPanel.getTabs()) {
+    			if (tab.getText().equals(title)) {
+    				mainTabPanel.getSelectionModel().select(tab);
+    			}
+    		}
+    	}
+    	
     	try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/reparacionesFormView.fxml"));
-			Parent reparacionFormView = loader.load();
+    		FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+			Parent content = loader.load();
 			
-			Tab reparacionFormTab = new Tab("Añadir Reparación");
-			reparacionFormTab.setContent(reparacionFormView);
+			Tab newTab = new Tab(title);
+			newTab.setContent(content);
 			
-			mainTabPanel.getTabs().add(reparacionFormTab);
-			mainTabPanel.getSelectionModel().select(reparacionFormTab);
-		} catch (IOException e) {
-			System.err.println("Error al abrir la vista de Reparaciones.");
+			mainTabPanel.getTabs().add(newTab);
+			mainTabPanel.getSelectionModel().select(newTab);
+		} catch (Exception e) {
+			System.err.println("Error al abrir la vista " + title);
 			e.printStackTrace();
 		}
-    }    
+    }
     
     public void setStatusMessage(StatusMessage statusMessage) {
     	Platform.runLater(() -> {
