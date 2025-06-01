744fdb5a1e5a1c9ee9a5221673878d14c67845fe
César Almeida <cesaralmeida89@gmail.com>
2025-06-01 09:28:56 +0200

    Add LoggerUtil class to handle file logging with console output suppression

---
+++ src/main/java/util/LoggerUtil.java
@@ -0,0 +1,43 @@
+package util;
+
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class LoggerUtil {
+	private static final Logger logger = Logger.getLogger(LoggerUtil.class.getName());
+	private static FileHandler fh;
+	
+	static {
+		try {
+			if (logger.getHandlers().length == 0) {
+				Logger rootLogger = Logger.getLogger("");
+	            for (java.util.logging.Handler handler : rootLogger.getHandlers()) {
+	                if (handler.getClass().getName().contains("ConsoleHandler")) {
+	                    rootLogger.removeHandler(handler);
+	                }
+	            }
+				
+			    fh = new FileHandler("vulcano-lite.log", true);
+			    fh.setFormatter(new SimpleFormatter());
+			    logger.addHandler(fh);
+			    logger.setLevel(Level.ALL);
+			}
+		} catch (Exception e) {
+			System.err.println("Logger init error: " + e.getMessage());
+		}
+	}
+	
+	public static void logError(String mensaje, Exception e) {
+        logger.log(Level.SEVERE, mensaje, e);
+    }
+	
+	public static void close() {
+		if (fh != null) {
+			fh.close();
+			logger.removeHandler(fh);
+			fh = null;
+		}
+	}
+}
